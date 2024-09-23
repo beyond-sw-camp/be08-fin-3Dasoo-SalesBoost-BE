7154c509cdeb770c49a41e6c127d0c3692a8f898
kuk329 <kuk329@cau.ac.kr>
2024-09-23 15:28:12 +0900

    initial spring project

---
+++ src/main/java/beyond/samdasoo/SalesBoostApplication.java
@@ -0,0 +1,13 @@
+package beyond.samdasoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesBoostApplication.class, args);
+    }
+
+}
