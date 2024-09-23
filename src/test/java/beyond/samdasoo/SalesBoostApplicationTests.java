eb031164e260013b31e968a9e82a7591e658a9cb
kuk329 <kuk329@cau.ac.kr>
2024-09-23 15:34:14 +0900

    chore : gitignore 수정

---
+++ src/test/java/beyond/samdasoo/SalesBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package beyond.samdasoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesBoostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
