f5e9df85ccb43edb616578028e3894aa5c06b726
uzz99 <yj.lee991228@gmail.com>
2024-09-27 23:14:08 +0900

    feat: #4 할 일 수정 및 삭제 API 추가
    - 생성 시 builder 적용
    - id 파라미터 변수 no로 변경

--- src/main/java/beyond/samdasoo/todo/service/TodoService.java
+++ src/main/java/beyond/samdasoo/todo/service/TodoService.java
@@ -4,12 +4,15 @@ import beyond.samdasoo.member.entity.Member;
 import beyond.samdasoo.member.repository.MemberRepository;
 import beyond.samdasoo.todo.dto.TodoRequestDto;
 import beyond.samdasoo.todo.dto.TodoResponseDto;
+import beyond.samdasoo.todo.dto.TodoUpdateDto;
 import beyond.samdasoo.todo.entity.Todo;
 import beyond.samdasoo.todo.repository.TodoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -30,17 +33,16 @@ public class TodoService {
         Member member = memberRepository.findById(todoRequestDto.getMemberNo())
                 .orElseThrow(() -> new EntityNotFoundException("회원 ID 조회 불가: " + todoRequestDto.getMemberNo()));
 
-        Todo todo = new Todo();
-        todo.setTitle(todoRequestDto.getTitle());
-        todo.setTodoCls(todoRequestDto.getTodoCls());
-        todo.setPriority(todoRequestDto.getPriority());
-        todo.setDueDate(todoRequestDto.getDueDate());
-        todo.setNotiYn(todoRequestDto.getNotiYn());
-        todo.setNotiDay(todoRequestDto.getNotiDay());
-        todo.setStatus(todoRequestDto.getStatus());
-        todo.setPrivateYn(todoRequestDto.getPrivateYn());
-        todo.setContent(todoRequestDto.getContent());
-        todo.setMemberNo(member);
+        Todo todo = Todo.builder()
+                .title(todoRequestDto.getTitle())
+                .todoCls(todoRequestDto.getTodoCls())
+                .priority(todoRequestDto.getPriority())
+                .dueDate(todoRequestDto.getDueDate())
+                .status(todoRequestDto.getStatus())
+                .privateYn(todoRequestDto.getPrivateYn())
+                .content(todoRequestDto.getContent())
+                .memberNo(member)
+                .build();
 
         todo = todoRepository.save(todo);
 
@@ -48,11 +50,30 @@ public class TodoService {
     }
 
     @Transactional(readOnly = true)
-    public TodoResponseDto getTodoById(Long id) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("할 일을 찾을 수 없습니다: " + id));
+    public TodoResponseDto getTodoById(Long no) {
+        Todo todo = todoRepository.findById(no)
+                .orElseThrow(() -> new RuntimeException("할 일을 찾을 수 없습니다: " + no));
         return new TodoResponseDto(todo);
     }
 
+    @Transactional
+    public void updateTodo(Long no, TodoUpdateDto todoUpdateDto) {
+        Todo todo = todoRepository.findById(no)
+                .orElseThrow(() -> new EntityNotFoundException("할 일을 찾을 수 없습니다: " + no));
 
+        Optional.ofNullable(todoUpdateDto.getTitle()).ifPresent(todo::setTitle);
+        Optional.ofNullable(todoUpdateDto.getTodoCls()).ifPresent(todo::setTodoCls);
+        Optional.ofNullable(todoUpdateDto.getPriority()).ifPresent(todo::setPriority);
+        Optional.ofNullable(todoUpdateDto.getDueDate()).ifPresent(todo::setDueDate);
+        Optional.ofNullable(todoUpdateDto.getPrivateYn()).ifPresent(todo::setPrivateYn);
+        Optional.ofNullable(todoUpdateDto.getContent()).ifPresent(todo::setContent);
+        Optional.ofNullable(todoUpdateDto.getStatus()).ifPresent(todo::setStatus);
+    }
+
+    public void deleteTodo(Long no) {
+        Todo todo = todoRepository.findById(no)
+                .orElseThrow(() -> new RuntimeException("할 일을 찾을 수 없습니다: " + no));
+
+        todoRepository.delete(todo);
+    }
 }
