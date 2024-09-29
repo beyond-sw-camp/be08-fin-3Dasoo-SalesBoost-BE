de768db5108566a10faa8fe82626c6efad347758
uzz99 <yj.lee991228@gmail.com>
2024-09-29 14:55:21 +0900

    docs: #4 Swagger 태그 및 API 문서 개선
    - 기본값 추가
    - 태그 및 엔드포인트 설명 추가

--- src/main/java/beyond/samdasoo/todo/controller/TodoController.java
+++ src/main/java/beyond/samdasoo/todo/controller/TodoController.java
@@ -4,12 +4,13 @@ import beyond.samdasoo.todo.dto.TodoRequestDto;
 import beyond.samdasoo.todo.dto.TodoResponseDto;
 import beyond.samdasoo.todo.dto.TodoUpdateDto;
 import beyond.samdasoo.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@Tag(name="todo", description = "할 일 API")
 @RestController
 @RequestMapping("/api/todo")
 public class TodoController {
@@ -21,24 +22,28 @@ public class TodoController {
     }
 
     @PostMapping
+    @Operation(summary = "할일 등록", description = "새로운 할 일 등록")
     public ResponseEntity<TodoResponseDto> createTodo(@RequestBody TodoRequestDto todoRequestDto) {
         TodoResponseDto responseDto = todoService.createTodo(todoRequestDto);
         return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/{no}")
+    @Operation(summary = "할일 조회", description = "특정 할 일 조회")
     public ResponseEntity<TodoResponseDto> getTodoById(@PathVariable("no") Long no) {
         TodoResponseDto responseDto = todoService.getTodoById(no);
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/{no}")
+    @Operation(summary = "할일 수정", description = "특정 할 일 수정")
     public ResponseEntity<Void> patchUpdateTodo(@PathVariable("no") Long no, @RequestBody TodoUpdateDto todoUpdateDto) {
         todoService.updateTodo(no, todoUpdateDto);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{no}")
+    @Operation(summary = "할일 삭제", description = "특정 할 일 삭제")
     public ResponseEntity<Void> deleteTodo(@PathVariable("no") Long no) {
         todoService.deleteTodo(no);
         return ResponseEntity.noContent().build();
