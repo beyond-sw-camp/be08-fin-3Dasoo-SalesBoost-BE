677af0a137ac8e4371937a58f168ec8726d5fc9b
uzz99 <yj.lee991228@gmail.com>
2024-09-30 21:56:11 +0900

    refact: Plan 기본 응답 형식 변경
    - 변수명 변경
    - docs: Swagger Api 문서 개선

--- src/main/java/beyond/samdasoo/plan/controller/PlanController.java
+++ src/main/java/beyond/samdasoo/plan/controller/PlanController.java
@@ -1,15 +1,22 @@
 package beyond.samdasoo.plan.controller;
 
+import beyond.samdasoo.common.response.BaseResponse;
 import beyond.samdasoo.plan.dto.PlanRequestDto;
 import beyond.samdasoo.plan.dto.PlanResponseDto;
 import beyond.samdasoo.plan.dto.PlanUpdateDto;
 import beyond.samdasoo.plan.service.PlanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static beyond.samdasoo.common.response.BaseResponseStatus.PLAN_NOT_EXIST;
+import static beyond.samdasoo.common.response.BaseResponseStatus.TODO_NOT_EXIST;
+
 @RestController
-@RequestMapping("/api/plan")
+@RequestMapping("/api/plans")
+@Tag(name="Plans APIs", description = "일정 API")
 public class PlanController {
     private final PlanService planService;
 
@@ -18,26 +25,38 @@ public class PlanController {
 
 
     @PostMapping
-    public ResponseEntity<PlanResponseDto> createPlan(@RequestBody PlanRequestDto planRequestDto) {
+    @Operation(summary = "일정 등록", description = "새로운 일정 등록")
+    public BaseResponse<PlanResponseDto> createPlan(@RequestBody PlanRequestDto planRequestDto) {
         PlanResponseDto responseDto = planService.createPlan(planRequestDto);
-        return ResponseEntity.ok(responseDto);
+        return new BaseResponse<>(responseDto);
     }
 
     @GetMapping("/{no}")
-    public ResponseEntity<PlanResponseDto> getPlanById(@PathVariable("no") Long no) {
+    @Operation(summary = "일정 조회", description = "특정 일정 조회")
+    public BaseResponse<PlanResponseDto> getPlanById(@PathVariable("no") Long no) {
         PlanResponseDto responseDto = planService.getPlanById(no);
-        return ResponseEntity.ok(responseDto);
+        return new BaseResponse<>(responseDto);
     }
 
     @PatchMapping("/{no}")
-    public ResponseEntity<Void> patchUpdatePlan(@PathVariable("no") Long no, @RequestBody PlanUpdateDto planUpdateDto) {
-        planService.updatePlan(no, planUpdateDto);
-        return ResponseEntity.noContent().build();
+    @Operation(summary = "일정 수정", description = "특정 일정 수정")
+    public BaseResponse<String> patchUpdatePlan(@PathVariable("no") Long no, @RequestBody PlanUpdateDto planUpdateDto) {
+        try{
+            planService.updatePlan(no, planUpdateDto);
+            return new BaseResponse<>("일정을 수정하였습니다.");
+        }catch (Exception e) {
+            return new BaseResponse<>(PLAN_NOT_EXIST);
+        }
     }
 
     @DeleteMapping("/{no}")
-    public ResponseEntity<Void> deletePlan(@PathVariable("no") Long no) {
-        planService.deletePlan(no);
-        return ResponseEntity.noContent().build();
+    @Operation(summary = "일정 삭제", description = "특정 일정 삭제")
+    public BaseResponse<String> deletePlan(@PathVariable("no") Long no) {
+        try{
+            planService.deletePlan(no);
+            return new BaseResponse<>("일정 삭제에 성공했습니다.");
+        }catch (Exception e) {
+            return new BaseResponse<>(PLAN_NOT_EXIST);
+        }
     }
 }
