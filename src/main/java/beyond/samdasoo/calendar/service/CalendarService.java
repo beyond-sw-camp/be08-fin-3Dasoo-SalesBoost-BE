f5d85d141fdba32e9d81fa236c7d9429a7c19266
uzz99 <yj.lee991228@gmail.com>
2024-10-04 22:25:43 +0900

    feat: #59 캘린더 등록, 조회, 수정 API 추가

---
+++ src/main/java/beyond/samdasoo/calendar/service/CalendarService.java
@@ -0,0 +1,72 @@
+package beyond.samdasoo.calendar.service;
+
+import beyond.samdasoo.calendar.dto.CalendarRequestDto;
+import beyond.samdasoo.calendar.dto.CalendarResponseDto;
+import beyond.samdasoo.calendar.entity.Calendar;
+import beyond.samdasoo.calendar.repository.CalendarRepository;
+import beyond.samdasoo.common.exception.BaseException;
+import beyond.samdasoo.common.response.BaseResponseStatus;
+import beyond.samdasoo.user.entity.User;
+import beyond.samdasoo.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static beyond.samdasoo.common.response.BaseResponseStatus.USER_NOT_EXIST;
+
+@Service
+public class CalendarService {
+    private final CalendarRepository cRepo;
+    private final UserRepository userRepository;
+
+    public CalendarService(CalendarRepository calendarRepository, UserRepository userRepository) {
+        this.cRepo = calendarRepository;
+        this.userRepository = userRepository;
+    }
+
+    public CalendarResponseDto createCalendar(CalendarRequestDto requestDto) {
+
+        User user = userRepository.findById(requestDto.getUserNo())
+                .orElseThrow(() -> new BaseException(USER_NOT_EXIST));
+
+        Calendar calendar = Calendar.builder()
+                .type(requestDto.getType())
+                .user(user)
+                .plans(new ArrayList<>())
+                .todos(new ArrayList<>())
+                .acts(new ArrayList<>())
+                .build();
+
+        calendar = cRepo.save(calendar);
+        return new CalendarResponseDto(calendar);
+    }
+
+    public CalendarResponseDto getCalendarByNo(Long no) {
+        Calendar calendar = cRepo.findCalendarById(no);
+        return new CalendarResponseDto(calendar);
+    }
+
+    public List<CalendarResponseDto> getAllCalendars() {
+        return cRepo.findAll().stream()
+                .map(CalendarResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void updateCalendar(Long no, CalendarRequestDto calendarRequestDto) {
+        Calendar calendar = cRepo.findCalendarById(no);
+        User user = userRepository.findById(calendarRequestDto.getUserNo())
+                .orElseThrow(() -> new BaseException(USER_NOT_EXIST));
+
+        calendar.setUser(user);
+        Optional.ofNullable(calendarRequestDto.getType())
+                .ifPresent(calendar::setType);
+
+        cRepo.save(calendar);
+    }
+
+}
