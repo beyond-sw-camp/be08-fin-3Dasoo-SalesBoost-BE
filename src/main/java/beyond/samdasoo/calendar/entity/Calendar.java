00e88b942c2b791725a6bd073f8250ff019517a9
uzz99 <yj.lee991228@gmail.com>
2024-10-04 22:23:36 +0900

    feat: #59 캘린더 엔티티 추가

---
+++ src/main/java/beyond/samdasoo/calendar/entity/Calendar.java
@@ -0,0 +1,45 @@
+package beyond.samdasoo.calendar.entity;
+
+import beyond.samdasoo.act.entity.Act;
+import beyond.samdasoo.plan.entity.Plan;
+import beyond.samdasoo.todo.entity.Todo;
+import beyond.samdasoo.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="tb_calendar")
+public class Calendar {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long no;
+
+        @Enumerated(EnumType.STRING)
+        @Column(name="calendar_type", nullable = false)
+        private CalendarType type;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name="user_no", nullable=false)
+        private User user;
+
+        @OneToMany(mappedBy = "calendar")
+        private List<Plan> plans;
+
+        @OneToMany(mappedBy = "calendar")
+        private List<Todo> todos;
+
+        @OneToMany(mappedBy = "calendar")
+        private List<Act> acts;
+}
+
