f46f408d7328b84b001f5266bffb501fdf87ff88
uzz99 <yj.lee991228@gmail.com>
2024-10-04 22:25:18 +0900

    feat: #59 캘린더 유형 enum 구현

---
+++ src/main/java/beyond/samdasoo/calendar/entity/CalendarType.java
@@ -0,0 +1,13 @@
+package beyond.samdasoo.calendar.entity;
+
+public enum CalendarType {
+    PLAN,
+    COMPLETE,
+    PERSONAL,
+    COMPANY,
+    PROPOSAL,
+    CONTRACT,
+    ESTIMATE,
+    TODO,
+
+}
