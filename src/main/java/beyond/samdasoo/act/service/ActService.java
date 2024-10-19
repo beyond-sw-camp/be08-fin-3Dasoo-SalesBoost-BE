db796196a36f685570188f5656a0f5485f954398
uzz99 <yj.lee991228@gmail.com>
2024-10-19 20:15:51 +0900

    feat: 영업활동 차트 관련 API 추가
    - 분류별 개수, 년도-월별 개수, 영업사원별 비율

--- src/main/java/beyond/samdasoo/act/service/ActService.java
+++ src/main/java/beyond/samdasoo/act/service/ActService.java
@@ -3,15 +3,20 @@ package beyond.samdasoo.act.service;
 import beyond.samdasoo.act.dto.ActRequestDto;
 import beyond.samdasoo.act.dto.ActResponseDto;
 import beyond.samdasoo.act.entity.Act;
+import beyond.samdasoo.act.entity.QAct;
 import beyond.samdasoo.act.repository.ActRepository;
 import beyond.samdasoo.calendar.repository.CalendarRepository;
 import beyond.samdasoo.common.exception.BaseException;
 import beyond.samdasoo.lead.Entity.Lead;
 import beyond.samdasoo.lead.repository.LeadRepository;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,11 +29,13 @@ public class ActService {
     private final ActRepository actRepository;
     private final LeadRepository leadRepository;
     private final CalendarRepository calendarRepository;
+    private final JPAQueryFactory queryFactory;
 
-    public ActService(ActRepository actRepository, LeadRepository leadRepository, CalendarRepository calendarRepository) {
+    public ActService(ActRepository actRepository, LeadRepository leadRepository, CalendarRepository calendarRepository, JPAQueryFactory queryFactory) {
         this.actRepository = actRepository;
         this.leadRepository = leadRepository;
         this.calendarRepository = calendarRepository;
+        this.queryFactory = queryFactory;
     }
 
     public Act findActById(Long no) {
@@ -93,4 +100,60 @@ public class ActService {
     }
 
     public void deleteAct(Long no) {actRepository.delete(findActById(no));}
+
+    public Map<String, Long> countActsCategory() {
+        QAct act = QAct.act;
+
+        return queryFactory
+                .select(act.cls, act.count())
+                .from(act)
+                .groupBy(act.cls)
+                .fetch()
+                .stream()
+                .collect(Collectors.toMap(
+                        tuple -> tuple.get(act.cls).name(),
+                        tuple -> tuple.get(act.count())
+                ));
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, Long> countMonthlyActs(int year) {
+        QAct act = QAct.act;
+        NumberTemplate<Integer> monthTemplate = Expressions.numberTemplate(Integer.class, "month({0})", act.actDate);
+        NumberTemplate<Integer> yearTemplate = Expressions.numberTemplate(Integer.class, "year({0})", act.actDate);
+
+        return queryFactory
+                .select(monthTemplate, act.count())
+                .from(act)
+                .where(yearTemplate.eq(year))
+                .groupBy(monthTemplate)
+                .fetch()
+                .stream()
+                .collect(Collectors.toMap(
+                        tuple -> year + "-" + String.format("%02d", tuple.get(monthTemplate)),
+                        tuple -> tuple.get(act.count())
+                ));
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, Double> activityRatio() {
+        QAct act = QAct.act;
+
+        long totalActivities = queryFactory
+                .selectFrom(act)
+                .fetchCount();
+
+        return queryFactory
+                .select(act.calendar.user.name, act.count())
+                .from(act)
+                .groupBy(act.calendar.user.name)
+                .fetch()
+                .stream()
+                .collect(Collectors.toMap(
+                        tuple -> tuple.get(act.calendar.user.name),
+                        tuple -> Math.round((double) tuple.get(act.count()) / totalActivities * 100 * 100) / 100.0
+                ));
+    }
+
+
 }
