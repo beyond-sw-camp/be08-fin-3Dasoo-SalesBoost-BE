e4ce4da1f9794c7a62ce344eb18d36399d36f4fe
kuk329 <kuk329@cau.ac.kr>
2024-10-21 15:20:18 +0900

    feat: #105 필터 조건 DTO

---
+++ src/main/java/beyond/samdasoo/customer/dto/SearchCriteriaDTO.java
@@ -0,0 +1,14 @@
+package beyond.samdasoo.customer.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class SearchCriteriaDTO {
+    private List<String> select; // 검색 조건
+    private String searchQuery; // 검색어
+    private String personInCharge; // 담당자
+    private String selectedKey; // 키맨 여부
+}
